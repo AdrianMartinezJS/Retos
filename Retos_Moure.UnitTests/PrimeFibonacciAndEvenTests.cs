68102660d83fdbae317f1565e23b18effaf2b240
Adrian Martinez <adrian.pdsm@gmail.com>
2023-03-02 23:13:22 +0100

    Add PrimeFibonacciAndEven Class and Tests

---
+++ Retos_Moure.UnitTests/PrimeFibonacciAndEvenTests.cs
@@ -0,0 +1,51 @@
+﻿using NUnit.Framework;
+using StringAssert = NUnit.Framework.StringAssert;
+
+namespace Retos_Moure.UnitTests
+{
+    [TestFixture]
+    public class PrimeFibonacciAndEvenTests
+    {
+        /*
+         * Escribe un programa que, dado un número, compruebe y muestre si es primo, fibonacci y par.
+         * Ejemplos:
+         * - Con el número 2, nos dirá: "2 es primo, fibonacci y es par"
+         * - Con el número 7, nos dirá: "7 es primo, no es fibonacci y es impar"
+         */
+        private PrimeFibonacciAndEven _numberToProof;
+
+        [SetUp]
+        public void SetUp () 
+        {
+            _numberToProof = new PrimeFibonacciAndEven();
+        }
+
+        [Test]
+        public void proof_whenCalledwithEvenNumber_returnsEvenNumber()
+        {
+            var result = _numberToProof.proof(4);
+            StringAssert.AreEqualIgnoringCase(result, "4 is not prime, is not Fibonacci and is pair");
+        }
+
+        [Test]
+        public void proof_whenCalledwithOddNumber_returnsOddNumber()
+        {
+            var result = _numberToProof.proof(9);
+            StringAssert.AreEqualIgnoringCase(result, "9 is not prime, is not Fibonacci and is unpair");
+        }
+
+        [Test]
+        public void proof_whenCalledwithPrimeAndOddNumber_returnsPrimeAndOddNumber()
+        {
+            var result = _numberToProof.proof(7);
+            StringAssert.AreEqualIgnoringCase(result, "7 is prime, is not Fibonacci and is unpair");
+        }
+
+        [Test]
+        public void proof_whenCalledwithPrimeFibonacciAndOddNumber_returnsPrimeFibonacciAndOddNumber()
+        {
+            var result = _numberToProof.proof(13);
+            StringAssert.AreEqualIgnoringCase(result, "13 is prime, is Fibonacci and is unpair");
+        }
+    }
+}
---
+++ Retos_Moure/PrimeFibonacciAndEven.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Retos_Moure
+{
+    public class PrimeFibonacciAndEven
+    {
+        private string _result = String.Empty;
+        private string _pair = String.Empty;
+        private string _prime = String.Empty;
+        private string _fibonacci = String.Empty;
+
+        private bool IsPrime(int num)
+        {
+            if (num <= 1)
+                return false;
+
+            for (int i = 2; i <= Math.Sqrt(num); i++)
+                if (num % i == 0)
+                    return false;
+
+            return true;
+        }
+
+        private bool IsFibonacci(int num)
+        {
+            int a = 0;
+            int b = 1;
+            while (b < num)
+            {
+                int temp = b;
+                b += a;
+                a = temp;
+            }
+            return b == num;
+        }
+
+        public string proof(int numberToProof) 
+        {
+            _pair = (numberToProof % 2 == 0) ? "pair" : "unpair";
+            _prime = (IsPrime(numberToProof)) ? "is" : "is not";
+            _fibonacci = (IsFibonacci(numberToProof)) ? "is" : "is not";
+
+            _result = String.Format("{0} {1} prime, {2} Fibonacci and is {3}", numberToProof, _prime, _fibonacci, _pair);
+            return _result;
+        }
+    }
+}
