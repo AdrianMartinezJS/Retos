851e10e5b31bfa66d3ceb696c310cdd0e44e8caa
Adrian Martinez <adrian.pdsm@gmail.com>
2023-03-05 22:39:29 +0100

    add rockPaperScissorsGame with tests

---
+++ Retos_Moure.UnitTests/RockPaperScissorsGameTests.cs
@@ -0,0 +1,73 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Assert = NUnit.Framework.Assert;
+using StringAssert = NUnit.Framework.StringAssert;
+
+namespace Retos_Moure.UnitTests
+{
+    [TestFixture]
+    public class RockPaperScissorsGameTests
+    {
+        /*
+         * Crea un programa que calcule quien gana más partidas al piedra,
+         * papel, tijera, lagarto, spock.
+         * - El resultado puede ser: "Player 1", "Player 2", "Tie" (empate)
+         * - La función recibe un listado que contiene pares, representando cada jugada.
+         * - El par puede contener combinaciones de "🗿" (piedra), "📄" (papel),
+         *   "✂️" (tijera), "🦎" (lagarto) o "🖖" (spock).
+         * - Ejemplo. Entrada: [("🗿","✂️"), ("✂️","🗿"), ("📄","✂️")]. Resultado: "Player 2".
+         * - Debes buscar información sobre cómo se juega con estas 5 posibilidades.
+         */
+        /* Reglas
+         * Tijera corta papel y lagarto
+         * Papel tapa piedra y a spock
+         * Piedra aplasta tijera y lagarto
+         * Lagarto come papel y a spock
+         * Spock rompe tijera y piedra
+         * */
+
+        private RockPaperScissorsGame _game;
+        private List<KeyValuePair<string, string>> _resultsOfGame;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _game = new RockPaperScissorsGame();
+            _resultsOfGame = new List<KeyValuePair<string, string>>();
+        }
+
+        [Test]
+        public void playGame_player1Winns_returnPlayer1Winns()
+        {
+
+            _resultsOfGame.Add(new KeyValuePair<string, string>("tijera", "papel"));
+            var result = _game.playGame(_resultsOfGame);
+            StringAssert.AreEqualIgnoringCase(result, "Player 1");
+        }
+
+        [Test]
+        public void playGame_player2Winns_returnPlayer2Winns()
+        {
+
+            _resultsOfGame.Add(new KeyValuePair<string, string>("spock", "lagarto"));
+            var result = _game.playGame(_resultsOfGame);
+            StringAssert.AreEqualIgnoringCase(result, "Player 2");
+        }
+
+        [Test]
+        public void playGame_Tie_returnTie()
+        {
+
+            _resultsOfGame.Add(new KeyValuePair<string, string>("spock", "lagarto"));
+            _resultsOfGame.Add(new KeyValuePair<string, string>("tijera", "papel"));
+            var result = _game.playGame(_resultsOfGame);
+            StringAssert.AreEqualIgnoringCase(result, "Tie");
+        }
+
+    }
+
+}
---
+++ Retos_Moure/RockPaperScissorsGame.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Retos_Moure
+{
+    /* Reglas
+    * Tijera corta papel y lagarto
+    * Papel tapa piedra y a spock
+    * Piedra aplasta tijera y lagarto
+    * Lagarto come papel y a spock
+    * Spock rompe tijera y piedra
+    * */
+    public class RockPaperScissorsGame
+    {
+        private string _result = String.Empty;
+        
+        
+        private string[] _ScissorsWinnsAgainst = new string[2] { "papel", "lagarto" };
+        private string[] _papelWinnsAgainst = new string[2] { "piedra", "spock" };
+        private string[] _RockWinnsAgainst = new string[2] { "tijera", "lagarto" };
+        private string[] _SpockWinnsAgainst = new string[2] { "tijera", "piedra" };
+        private string[] _lizardWinnsAgainst = new string[2] { "papel", "spock" };
+        private Dictionary<string, string[]> _rules= new Dictionary<string, string[]>();
+        
+        public RockPaperScissorsGame()
+        {
+            _rules.Add("piedra", _RockWinnsAgainst);
+            _rules.Add("papel", _papelWinnsAgainst);
+            _rules.Add("tijera", _ScissorsWinnsAgainst);
+            _rules.Add("lagarto", _lizardWinnsAgainst);
+            _rules.Add("spock", _SpockWinnsAgainst);
+        }
+        public string playGame(List<KeyValuePair<string, string>> game)
+        {
+            var _player1 = new Player();
+            var _player2 = new Player();
+
+            foreach (var round in game)
+            {
+                if (round.Key == round.Value)
+                    Console.WriteLine("Tie");
+                else
+                {
+                    if (round.Value == _rules[round.Key][0] || round.Value == _rules[round.Key][1])
+                        _player1.incrementPoints();
+                    if (round.Key == _rules[round.Value][0] || round.Key == _rules[round.Value][1])
+                        _player2.incrementPoints();
+                }
+            }
+            if (_player1.Points > _player2.Points)
+                _result = "Player 1";
+            else if (_player1.Points < _player2.Points)
+                _result = "Player 2";
+            else
+                _result = "Tie";
+            return String.Format("{0}", _result);
+        }
+    }
+}
